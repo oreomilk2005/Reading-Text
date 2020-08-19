a406934e9d7c1a6c0cfb8d0783cad26c338a00c8
Olivia Wu <59664703+oreomilk2005@users.noreply.github.com>
2020-08-19 02:17:13 -0700

    Add files via upload

--- ReadingText/src/Main.java
+++ ReadingText/src/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -14,22 +15,25 @@ public class Main {
             lines.add(line);
         }
         br.close();
+        Scanner scan = new Scanner(System.in);
+        String word = scan.next();
+        search(word, lines);
     }
 
     public static void search(String word, ArrayList<String> lines){
         int found = 0;
         System.out.print("The word is ");
-        //For every lines, for every string of length word length inside the line, if the word equals to the string and the next character is a space or symbol, then we found an additional word.
-        //We record the finding by increasing the integer found and print the index position of the line and word
+        //For every lines, for every string of length word length inside the line, if the string equals the word, then we found an additional word.
+        //We record the finding by increasing the integer found and print the index position of the line and word position that the word is in.
         for(String s:lines){
             for(int j=0; j<s.length()-word.length(); j++){
-                if(s.substring(j, j+word.length()).equals(word) && s.charAt(j+word.length())<=64){
+                if(s.substring(j, j+word.length()).equals(word)){
                     found ++;
                     System.out.print("\nIn index position of: Line: "  + lines.indexOf(s) + " Position: " + j + " to " + (j+word.length()));
                 }
             }
         }
-        //if nothing was found, then we print "not found"
+        //If nothing was found, then we print "not found"
         if(found == 0) System.out.print("not found.");
     }
 }
